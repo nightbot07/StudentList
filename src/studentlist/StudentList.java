0693a4ebe6a98994ea7242ad9cf17bb559de38ad
Dharmik Patel <patel93@sheridancollege.ca>
2025-01-22 10:55:55 -0500

    changes

--- src/studentlist/StudentList.java
+++ src/studentlist/StudentList.java
@@ -22,8 +22,9 @@ public class StudentList {
         s2.setName("Palak Shah");
         Student s3 = new Student();
         s3.setName("Megh Vankawala");
-//        Student s4 = new Student();
-//        s4.setName("Megh Vankawala");
+        
+   Student s4 = new Student();
+        s4.setName("Megh Vankawala");
 //       
         studentList[0] = s1;
         studentList[1] = s2;
